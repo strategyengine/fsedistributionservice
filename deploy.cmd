e5b2acaa0ca83f4b5753a8c0f0d2d05dc6fd8f80
pops <barry25@hotmail.com>
2022-09-29 13:43:04 -0400

    cleanup unused files

--- deploy.cmd
+++
@@ -1,1 +0,0 @@
-mvn clean compile install && docker build -t fsedistributionservice . &&  docker tag fsedistributionservice gcr.io/flarestrategyengine/fsedistributionservice && docker push gcr.io/flarestrategyengine/fsedistributionservice
--- deploy.sh
+++
@@ -1,8 +0,0 @@
-docker rmi fsedistributionservice:latest
-rm ../fsedistributionservice.tar.gz
-mvn clean compile install
-docker build -t fsedistributionservice:latest .
-docker save fsedistributionservice:latest | gzip > ../fsedistributionservice.tar.gz
-pscp -pw 1359 ../fsedistributionservice.tar.gz  pops@10.0.0.33:/home/pops/docker/deploys
-#pscp -pw 1359 ../fsedistributionservice.tar.gz  pops@10.0.0.31:/home/pops/docker/deploys
-
